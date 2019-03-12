bb7fed24e6448e2d50699ed4989493176f8ab4ec
Nicholas <nicholas.zx.99@gmail.com>
2019-03-12 15:45:38 +0100

    second part

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 function sendButton() {
   if($(".write-input").val() != "") {
 
@@ -16,9 +10,7 @@ function sendButton() {
   }
 }
 
-
-function sendMessage() {
-
+function getMyBox() {
   var textInput = $(".write-input").val();
   var box = document.createElement("div");
   box.className = box.classList + (" message-box");
@@ -27,17 +19,75 @@ function sendMessage() {
   var text = document.createElement("span");
   text.innerHTML = textInput;
   box.appendChild(mes).appendChild(text);
-  $(".chat").append(box);
+  return box;
+}
 
-  $(".write-input").val("");
-  sendButton();
+function getRispBox() {
+  var textInput = "OK";
+  var box = document.createElement("div");
+  box.className = box.classList + (" message-box");
+  var mes = document.createElement("div");
+  mes.className = mes.classList + (" message rispMes");
+  var text = document.createElement("span");
+  text.innerHTML = textInput;
+  box.appendChild(mes).appendChild(text);
+  return box;
+}
+
+function getAns() {
+
+  var box = getRispBox();
+  $(".chat").append(box);
+}
+
+function sendMessage() {
+
+  if($(".write-input").val() != "") {
+
+    var box = getMyBox();
+    $(".chat").append(box);
+
+    $(".write-input").val("");
+    sendButton();
+
+    setTimeout(getAns, 1000);
+  }
+
+}
+
+function showSearched() {
+
+  $(".prev").show();
+
+  var searched = $(".search-input").val();
+
+  for (var i = 0; i < $(".prev").length; i++) {
+
+    var prev = $(".prev").eq(i);
+
+    var toCheck = prev.find(".name-chat span").text();
+
+    if(!toCheck.includes(searched)) {
+      prev.hide();
+    }
+
+  }
 }
 
 function init() {
   $(".fa-chevron-circle-right").hide();
+
+  $(".search-input").on("input", showSearched);
+
   $(".write-input").on("input", sendButton);
 
-  $(".fa-chevron-circle-right").on("click", sendMessage);
+  $(".fa-chevron-circle-right").click(sendMessage);
+  $(document).keydown(function(e) {
+    if(e.which == 13) {
+      sendMessage();
+    }
+  });
+
 }
 
-$(init)
+$(init);
